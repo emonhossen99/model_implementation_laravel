b476b0d66a6f921bb11a871474c21922f1f242e3
ahamedemon99 <mdxhamedemon@gmail.com>
2023-01-26 18:57:52 +0600

    laravel all commaed added

---
+++ model.php
@@ -0,0 +1,29 @@
+<?php
+
+// all model command in laravel 
+
+// 1.Make Model 
+// php artisan make:model Model Name
+
+// 2. Make Controller 
+// php artisan make:controller Controller Name
+
+// 3. Make Resource  Controller 
+// php artisan make:controller Controller Name --resource 
+
+// 4. Get Resource  Controller All Route
+// php artisan route:list
+
+// 5.create Table
+// php artisan make:migration create_TableName_table
+
+// 6. created table inset in database
+// php artisan migrate
+
+// 7. 
+// 
+
+
+
+
+?>
